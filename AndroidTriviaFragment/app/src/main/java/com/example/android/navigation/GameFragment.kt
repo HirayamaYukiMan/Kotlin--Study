ae4237cd783483335f73f78667bf8753ed7c8723
HirayamaYukiMan <hirasukon@iCloud.com>
2022-06-06 22:40:57 +0900

    Toastをインポート

--- AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -103,16 +104,20 @@ class GameFragment : Fragment() {
 
                         view.findNavController()
                             // .navigate(R.id.action_gameFragment_to_gameWonFragment)
-                            .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     view.findNavController().
-                    //navigate(R.id.action_gameFragment_to_gameOverFragment)
-                        .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
+                        //navigate(R.id.action_gameFragment_to_gameOverFragment)
+                    navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
+
                 }
             }
         }
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 
