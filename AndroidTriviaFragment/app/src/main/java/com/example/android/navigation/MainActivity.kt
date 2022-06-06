a3c157d4408bb2b4e9790e4b3d78840d5db854cb
HirayamaYukiMan <hirasukon@iCloud.com>
2022-06-06 22:15:34 +0900

    プラグイン（SafeArg）を追加

--- AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -31,10 +31,11 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
+        drawerLayout = binding.drawerLayout
         NavigationUI.setupActionBarWithNavController(this,navController)
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        drawerLayout = binding.drawerLayout
+
 
     }
     override fun onSupportNavigateUp(): Boolean {
